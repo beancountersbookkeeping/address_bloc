2b4499186169c6439ec5a923862d62f180f43750
Kaylin Leland <kay@thebeancountersbookkeeping.com>
2017-05-26 11:45:06 +0000

    Checkpoint6

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook 
     attr_reader :entries
@@ -10,7 +11,7 @@ class AddressBook
   def remove_entry(name, phone_number, email)
       delete_entry = nil 
       
-      @entries.each do (entry)
+      @entries.each do |entry|
         if name == entry.name && phone == entry.phone_number && email == entry.email 
           delete_entry = entry
         end 
@@ -28,4 +29,17 @@ class AddressBook
      end
      entries.insert(index, Entry.new(name, phone_number, email))
   end 
+  
+  def import_from_csv(file_name)
+ 
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+     
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+      
+  end
+  
 end
